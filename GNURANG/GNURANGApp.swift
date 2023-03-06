dfd2b07ebe11e00459a7d26d2fa12a183e6e6143
i1hwan <mtslzx@gmail.com>
2023-03-06 16:18:47 +0900

    Initial Commit

---
+++ GNURANG/GNURANGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNURANGApp.swift
+//  GNURANG
+//
+//  Created by 하일환 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct GNURANGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
